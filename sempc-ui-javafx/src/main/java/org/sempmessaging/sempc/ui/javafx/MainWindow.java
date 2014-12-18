50e0ad20793a70702381d320b23aa2d63e950783
David Tanzer <david@davidtanzer.net>
2014-12-18 06:05:33 +0100

    sempc now starts the main window of the application - which does nothing for now.

---
+++ sempc-ui-javafx/src/main/java/org/sempmessaging/sempc/ui/javafx/MainWindow.java
@@ -0,0 +1,7 @@
+package org.sempmessaging.sempc.ui.javafx;
+
+public class MainWindow {
+	public void show() {
+		
+	}
+}
--- sempc/src/main/java/org/sempmessaging/sempc/SEMPClient.java
+++ sempc/src/main/java/org/sempmessaging/sempc/SEMPClient.java
@@ -1,12 +1,19 @@
 package org.sempmessaging.sempc;
 
-public class SEMPClient {
-	public SEMPClient() {
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.sempmessaging.sempc.ui.javafx.MainWindow;
 
+public class SEMPClient {
+	private final Injector injector;
+
+	public SEMPClient() {
+		injector = Guice.createInjector();
 	}
 
 	public void start() {
-
+		MainWindow mainWindow = injector.getInstance(MainWindow.class);
+		mainWindow.show();
 	}
 
 	public static void main(String[] args) {
