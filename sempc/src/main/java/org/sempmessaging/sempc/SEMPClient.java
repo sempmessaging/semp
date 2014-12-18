4724e372113663d710fedc9cd500dee8a58b5612
David Tanzer <david@davidtanzer.net>
2014-12-18 06:01:28 +0100

    The project now contains all modules we need for SEMPC right now.

---
+++ sempc/src/main/java/org/sempmessaging/sempc/SEMPClient.java
@@ -0,0 +1,17 @@
+package org.sempmessaging.sempc;
+
+public class SEMPClient {
+	public SEMPClient() {
+
+	}
+
+	public void start() {
+
+	}
+
+	public static void main(String[] args) {
+		SEMPClient client = new SEMPClient();
+		client.start();
+	}
+
+}
