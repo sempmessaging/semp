b9f21e0e98aa6c9636b61b90c3189988c89a50c6
David Tanzer <david@davidtanzer.net>
2015-01-23 08:35:59 +0100

    The split pane now defines all it's styles in the css file.

--- sempc-ui/src/main/java/org/sempmessaging/sempc/ui/HtmlSplitPane.java
+++ sempc-ui/src/main/java/org/sempmessaging/sempc/ui/HtmlSplitPane.java
@@ -6,8 +6,6 @@ import net.davidtanzer.html.values.CssClass;
 import net.davidtanzer.html.values.CssStyle;
 
 public class HtmlSplitPane extends HtmlComponent {
-	private int dividerLocation = 30;
-
 	public HtmlSplitPane() {
 		cssClass(new CssClass("split-pane"));
 		cssClass(new CssClass("horizontal-split"));
@@ -17,11 +15,9 @@ public class HtmlSplitPane extends HtmlComponent {
 	protected FlowContentNode[] getInnerHtml() {
 		Div first = new Div();
 		first.cssClass(new CssClass("first"));
-		first.style(new CssStyle("width: "+dividerLocation+"%;"));
 
 		Div second = new Div();
 		second.cssClass(new CssClass("second"));
-		second.style(new CssStyle("width: calc("+(100-dividerLocation)+"% - 2px);"));
 
 		Div divider = new Div();
 		divider.cssClass(new CssClass("divider"));
